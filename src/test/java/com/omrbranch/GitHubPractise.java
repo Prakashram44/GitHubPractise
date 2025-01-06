7a699dff547bf9d6a60d90405ac2b6ed2b7c6699
Prakashram44 <divyaprakash4493@gmail.com>
2025-01-06 17:05:11 +0530

    Project Created

---
+++ src/test/java/com/omrbranch/GitHubPractise.java
@@ -0,0 +1,12 @@
+package com.omrbranch;
+
+public class GitHubPractise {
+	private void savings() {
+		System.out.println("Savings");
+		
+	}
+
+	private void deposit() {
+		System.out.println("Deposit");	
+	}	
+}
